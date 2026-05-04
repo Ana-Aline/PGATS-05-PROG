1b1961b58591f2c60f3ca90736ca8f31a5a23f18
Ana-Aline <anaalinedourado@outlook.com>
2026-05-04 15:42:15 -0300

    First commit from Aula5

---
+++ passos.md
@@ -0,0 +1,22 @@
+1. Abra a pasta do projeto
+2. Clique Terminal
+3. Clique em New Terminal
+4. Digite: npm init -y
+5. Pressione ENTER
+6. Crie uma pasta “src”
+7. Crie uma pasta “test”
+8. Abra o arquivo package.json
+9. Altere o valor da propriedade “type” e “commonjs” para “module”
+10. Instale o package “mocha” no seu projeto digitando npm i mocha no Terminal
+
+# exercício:
+
+// 1) Criar um vetor com 5 livros contendo as seguintes propriedades: ID, titulo, tema, preco.
+
+// 2) [Básico] Crie uma função que receba o ID de um livro e retorne o seu titulo. Exemplo de chamada da função:
+// buscarTituloDoLivroPorID(99) -> 'Titulo do Livro de ID 99'
+// 3) Escreva um test automatizado de unidade para testar a condição de busca do titulo por ID.
+
+// 4) [Avançado] Validar se o ID foi informado e é maior que 0.
+// 5) Escreva dois testes automatizados de unidade para testar as condições de envio incorreto de ID.
+// 6) Adicionar o Mochawesome para gerar relatórios em HTML.
---
+++ test/gestaoProdutos.test.js
@@ -0,0 +1,46 @@
+import { consultarTituloPorId } from "../src/gestaoProdutos.js";
+import assert, { AssertionError } from "node:assert";
+
+describe("Testes de gestão de produto", function () {
+  describe("Testes de consulta de produto", function () {
+    it("Validar consultar título está retornando o título o correto", function () {
+      //arrange
+      const idProduto = 1;
+      const tituloProduto = "livro 1";
+
+      //act
+      const tituloRetornado = consultarTituloPorId(idProduto);
+
+      //assert
+      assert.equal(tituloProduto, tituloRetornado);
+    });
+    it("Validar se o ID foi informado e é maior que 0", function () {
+      //arrange
+      const idProduto = 0;
+      const mensagemEsperada = "O id deve ser maior que 0";
+
+      //act - assert
+      assert.throws(
+        () => {
+          consultarTituloPorId(idProduto);
+        },
+        Error,
+        mensagemEsperada,
+      );
+    });
+    it("Validar se o ID existe", function () {
+      //arrange
+      const idProduto = 100;
+      const mensagemEsperada = "Produto inexistente";
+
+      //act - assert
+      assert.throws(
+        () => {
+          consultarTituloPorId(idProduto);
+        },
+        Error,
+        mensagemEsperada,
+      );
+    });
+  });
+});
